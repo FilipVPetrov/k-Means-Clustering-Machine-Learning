40fc94bf17045405d17597dcce20b63b4e83f4e7
Filip Petrov <lombert.p.v@gmail.com>
2017-01-12 14:17:12 +0200

    src folder

---
+++ src/Color.py
@@ -0,0 +1,13 @@
+
+class Color(object):
+    colors = {
+        "blue" : "b",
+        "red" : "r",
+        "magenta" : "m",
+
+        "cyan" : "c",
+        "yellow" : "y",
+        "black" : "k",
+        "white" : "w"
+    }
+#         "green" : "g",
